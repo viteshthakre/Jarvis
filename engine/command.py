17390fb0e49bf269bb26e45cd2033f494e0a48fe
Vitesh2004 <156696502+Vitesh2004@users.noreply.github.com>
2025-05-30 10:28:33 +0530

    Whatsapp automation done

--- engine/command.py
+++ engine/command.py
@@ -1,9 +1,11 @@
+
 import pyttsx3
 import speech_recognition as sr
 import eel
 import time
 
 def speak(text):
+   text = str(text)
    engine = pyttsx3.init('sapi5')
    voices = engine.getProperty('voices')
    engine.setProperty('voice', voices[1].id)
@@ -11,6 +13,7 @@ def speak(text):
    eel.DisplayMessage(text)  
 #    print(voices)
    engine.say(text)
+   eel.receiverText(text)
    engine.runAndWait()
 
 
@@ -44,18 +47,49 @@ def takecommand():
 
 @eel.expose
 
-def allCommands():
+def allCommands(message=1):
     
-    query = takecommand()
-    print(query)
     
-    if "open" in query:
-        from engine.features import openCommand
-        openCommand(query)
-    elif "on yputube":  
-        from engine.features import PlayYoutube
-        PlayYoutube(query)
+    if message==1:
+        query = takecommand()
+        print(query)
+        eel.senderText(query)
     else:
-         print("not run")   
-         
+        query= message    
+        eel.senderText(query)
+    try:
+        
+    
+        if "open" in query:
+            from engine.features import openCommand
+            openCommand(query)
+        elif "on youtube" in query:  
+            from engine.features import PlayYoutube
+            PlayYoutube(query)
+            
+        elif "send message" in query or "phone call" in query or "video call" in query:
+            from engine.features import findContact, whatsApp
+            flag = ""
+            contact_no, name = findContact(query)
+            if(contact_no != 0):
+
+                if "send message" in query:
+                    flag = 'message'
+                    speak("what message to send")
+                    query = takecommand()
+
+                elif "phone call" in query:
+                    flag = 'call'
+                else:
+                    flag = 'video call'
+
+                whatsApp(contact_no, query,flag,name)
+                    
+        else:
+            from engine.features import chatBot
+            chatBot(query)
+            # print("not run")   
+    except:
+        print("error")
+             
     eel.ShowHood()     
--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import eel
 
 from engine.features import *
 from engine.command import *
-eel.init("www")
 
-playAssistantSound()
-os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
-eel.start('index.html' , mode=None, host='localhost',block=True)
+def start():
+    
+    eel.init("www")
+
+    playAssistantSound()
+
+
+    os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+    eel.start('index.html' , mode=None, host='localhost',block=True)
